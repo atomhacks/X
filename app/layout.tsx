eec43c3f16c41d4fea8a1a8284d3cf852e14e314
Fahim Hussain <fahimhussain21@gmail.com>
2024-02-05 23:26:31 -0500

    inshallah

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import React from "react";
 import { Lexend_Deca } from "next/font/google";
 import "./globals.css";
-import { Session } from "next-auth";
 import { Providers } from "./components/Providers";
 
 const font = Lexend_Deca({ subsets: ["latin"] });
@@ -11,13 +10,13 @@ export const metadata: Metadata = {
   description: "Bronx Science's 10th Annual Hackathon",
 };
 
-export default function RootLayout({ children, session }: { children: React.ReactNode; session?: Session }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/* Puting the Navbar here allows it to be replicated across all pages, including
       the main page and the gallery page. Think of this as a template for all pages.*/}
       <body className={font.className}>
-        <Providers session={session}>{children}</Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
