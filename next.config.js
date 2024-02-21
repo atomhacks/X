f0998c33909738944617cddb1ff2a1a1f4cf47ba
David Wang <wangd7@bxscience.edu>
2024-02-21 13:10:06 -0500

    fix: Secure next.config.js (lint CI/CD operational)

--- next.config.js
+++ next.config.js
@@ -3,14 +3,4 @@ module.exports = {
   images: {
     remotePatterns: [{ hostname: "*.public.blob.vercel-storage.com" }, {hostname: "lh3.googleusercontent.com"}, {hostname: "tr.rbxcdn.com"}, {hostname: "static.wikia.nocookie.net"}],
   },
-	eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 }
