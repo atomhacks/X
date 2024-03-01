e521f6d24027aeb1ee4e5afd90ede5e630910bee
wd7bxscience <wangd7@bxscience.edu>
2024-03-01 01:36:26 -0500

    fix: Do not allow submissions page to cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    remotePatterns: [{ hostname: "*.public.blob.vercel-storage.com" }, {hostname: "lh3.googleusercontent.com"}, {hostname: "tr.rbxcdn.com"}, {hostname: "static.wikia.nocookie.net"}],
+    remotePatterns: [
+      { hostname: "*.public.blob.vercel-storage.com" },
+      { hostname: "lh3.googleusercontent.com" },
+      { hostname: "tr.rbxcdn.com" },
+      { hostname: "static.wikia.nocookie.net" },
+    ],
   },
-}
+  headers: () => [
+    {
+      source: "/dashboard/submissions",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store",
+        },
+      ],
+    },
+  ],
+};
