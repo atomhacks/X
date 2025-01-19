e5cfa9217a79deaeeca491e6f6af559ea6fd91f3
Audrey Zhu <aazhu615@gmail.com>
2025-01-19 12:32:43 -0500

    Updated home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,17 +7,15 @@ import "./globals.css";
 
 const font = Lexend_Deca({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: "AtomHacks X",
-  description: "Bronx Science's 10th Annual Hackathon",
+  title: "AtomHacks XI",
+  description: "Bronx Science's 11th Annual Hackathon",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <body className={font.className}>
-        {children}
-      </body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
