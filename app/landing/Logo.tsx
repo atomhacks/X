edfcc2199248dd2e06bfc9aac557f28bfdd66a5b
Audrey Zhu <aazhu615@gmail.com>
2025-03-24 19:48:12 -0400

    fixed logo background

--- app/landing/Logo.tsx
+++ app/landing/Logo.tsx
@@ -3,8 +3,8 @@
 export default function Logo() {
   return (
     <div className="relative bottom-8">
-      <div className=" mt-[80px] h-[22rem] w-[22rem] justify-center md:h-[40rem] md:w-[40rem]">
-        <img src="/rive/logo2025.png" alt="logo" />
+      <div className=" mt-[80px] flex h-[22rem] w-[22rem] items-center justify-center md:h-[40rem] md:w-[40rem]">
+        <img src="/rive/Atom Hacks Logo.png" alt="logo" />
       </div>
     </div>
   );
